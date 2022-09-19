855ece73df8e0312b460c38dd9085b512d084635
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-19 20:57:16 +0300

    render

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,14 +1,37 @@
-export { fetchPictures };
+export default class SearchApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 40;
+    this.baseUrl = 'https://pixabay.com/api/';
+    this.key = '30026905-3ea175f3d607ccbd72fb69757';
+  }
 
-const BACE_URL = 'https://pixabay.com/api/';
-const KEY = '30026905-3ea175f3d607ccbd72fb69757';
-const PER_PAGE = 10;
-const PAGE = 1;
+  fetchPictures() {
+    console.log(this);
+    const url = `${this.baseUrl}/?key=${this.key}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+
+        return data.hits
+        // , data.totalHits;
+      });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
 
 // https://pixabay.com/api/?key={ KEY }&q=yellow+flowers&image_type=photo
-
-
-function fetchPictures(searchQuery) {
-  const url = `${BACE_URL}/?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PER_PAGE}&page=${PAGE}`;
-  return fetch(url).then(response => response.json());
-}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import axios from 'axios';
-import { fetchPictures } from './fetchPictures';
+// import { fetchPictures } from './fetchPictures';
+import SearchApiService from './fetchPictures';
 
 const refs = {
   //   body: document.querySelector('body'),
   formField: document.querySelector('form'),
-  inputField: document.querySelector('input'),
-  //   coutryInfo: document.querySelector('.country-info'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  galleryContainer: document.querySelector('.gallery'),
 };
 
+const searchApiService = new SearchApiService();
+
 refs.formField.addEventListener('submit', onSearchName);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearchName(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.elements.searchQuery.value;
-  console.log(searchQuery);
-  fetchPictures(searchQuery);
+  searchApiService.query = event.currentTarget.elements.searchQuery.value;
+  searchApiService.resetPage();  
+  searchApiService.fetchPictures().then(renderHits);
+
 }
 
-//   fetchCountries(q.trim())
-//     .then(q => render(q))
-//     .catch(onError);
+function onLoadMore() {
+  searchApiService.fetchPictures().then(hits => console.log(hits));
+}
+
+
+function renderHits(hits) {
+  refs.galleryContainer.insertAdjacentHTML(
+    'beforeend',
+    `<div class="photo-card">
+  <img src="${hits}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`
+  );
+}
