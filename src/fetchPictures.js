faee6b6860a6e531cd8ab9e7a1c42a0d311d87dd
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-09-24 15:13:10 +0300

    Update fetchPictures.js

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -13,7 +13,7 @@ export default class SearchApiService {
   async fetchPictures() {
     const url = `${BACE_URL}/?key=${KEY}&q=${this.searchQuery}
     &image_type=photo&orientation=horizontal
-    &safesearch=true&per_page=5&page=${this.page}`;
+    &safesearch=true&per_page=40&page=${this.page}`;
     try {
       const response = await axios.get(url);
       const data = response.data;
